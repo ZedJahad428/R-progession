ed92c880de65fe2c6db21bf58f7e2809ae8cb366
ZedJahad428 <68990535+ZedJahad428@users.noreply.github.com>
2025-06-19 12:01:51 +0200

    Create project2.r
    Day 2

---
+++ project2.r
@@ -0,0 +1,14 @@
+A <- 10
+B <- 5
+
+addition <- A + B
+subtraction <- A - B
+multiplication <- A * B
+division <- A / B
+powerof <- A ^ B
+
+print("A + B = ", addition, '\n')
+print("A - B = ", subtraction, '\n')
+print("A * B = ", multiplication, '\n')
+print("A / B = ", division, '\n')
+print("A ^ B = ", powerof, '\n')
